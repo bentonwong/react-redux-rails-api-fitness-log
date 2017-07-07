c0df3969f6cac26672ba8ee5c92d015f1596c731
bentonwong <bentonwong@gmail.com>
2017-07-07 13:21:55 -0700

    add header

--- react-redux-client/src/index.js
+++ react-redux-client/src/index.js
@@ -7,6 +7,24 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import App from './components/App';
 import reducers from './reducers';
 
+class Hello extends React.Component {
+  render() { return <div>Hello!</div> }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Goodbye!</div> }
+}
+
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <BrowserRouter>
+      <div>
+        <header>My Fitness Log</header>
+        <Route path="/hello" component={Hello}/>
+        <Route path="/goodbye" component={Goodbye}/>
+      </div>
+    </BrowserRouter>
+  </Provider>
+  , document.getElementById('root'));
