f8fc050a20e3083a68ef00a8768a6ebf38485184
bentonwong <bentonwong@gmail.com>
2017-07-07 12:52:58 -0700

    setup index.js

--- react-redux-client/src/index.js
+++ react-redux-client/src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
