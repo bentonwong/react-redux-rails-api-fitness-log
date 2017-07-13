08482e5544ccc37a0a204494ffa16ae10c5a87ab
bentonwong <bentonwong@gmail.com>
2017-07-13 08:56:57 -0700

    refactor Chart component

--- react-redux-client/src/components/chart.js
+++ react-redux-client/src/components/chart.js
@@ -6,15 +6,19 @@ export default (props) => {
   return (
     <div>
       <Sparklines data={props.data} limit={30} svgWidth={500} svgHeight={200}>
-        <SparklinesLine color={props.color} />
-        <SparklinesReferenceLine type="avg" />
+        <SparklinesLine style={{ fill: "none" }} color={props.color}/>
         <SparklinesNormalBand />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
-      <div className="center-text">{isNaN(average(props.data)) ? 'calculating average...' : "Average: " + average(props.data) + ` ${props.units}`}</div>
+      <div className="center-text">{displayAverage(props)}</div>
     </div>
   );
 }
 
 function average(data) {
   return _.round(_.sum(data)/data.length);
+}
+
+function displayAverage(props) {
+  return isNaN(average(props.data)) ? 'calculating average...' : "Average: " + average(props.data) + ` ${props.units}`
 }
